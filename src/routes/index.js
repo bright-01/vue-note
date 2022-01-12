06936abbc479da1555676dde45735c74d49a1090
mwchoen <mwcheon@cudo.co.kr>
2022-01-12 10:01:25 +0900

    test

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/LoginPage'),
+      // 코드 스플리팅 : 처음에 모든 페이지를 로딩해서 가져오지 않고 해당 페이지만 동적으로 그때 그때 가져오는 방법
+    },
+    {
+      path: '/signup',
+      component: () => import('@/views/SignupPage'),
+    },
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage'),
+    },
+  ],
+});
