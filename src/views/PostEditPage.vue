dbcc02235959d748386060e9747dcb56f755c5c8
mwchoen <cheonmyeongwon@gmail.com>
2022-01-25 11:53:32 +0900

    post edit

---
+++ src/views/PostEditPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="form-container">
+    <PostEditForm></PostEditForm>
+  </div>
+</template>
+
+<script>
+import PostEditForm from '@/components/posts/PostEditForm';
+
+export default {
+  name: 'PostEditPage',
+  components: {
+    PostEditForm,
+  },
+};
+</script>
+
+<style scoped></style>
