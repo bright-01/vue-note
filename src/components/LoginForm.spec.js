94614d4bfb81de5d68c2966e620a0cd800027d83
mwchoen <cheonmyeongwon@gmail.com>
2022-01-26 09:41:41 +0900

    jest 테스트 예

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -13,12 +13,49 @@
 // });
 
 import LoginForm from '@/components/LoginForm';
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
+// import Vue from 'vue';
 describe('LoginForm.vue', () => {
   test('컴포넌트가 마운팅 되면 화면에 그려져야 한다. ', () => {
-    const instance = new Vue(LoginForm).$mount(); //인스턴스가 마운트 되는 것을 확인
-    console.log(instance.username);
+    // const instance = new Vue(LoginForm).$mount(); //인스턴스가 마운트 되는 것을 확인
+    // console.log(instance.username);
+
+    const wrapper = shallowMount(LoginForm); // << 위의 코드 2줄을 shallowMount로 줄일 수 있다.
+    expect(wrapper.vm.username);
     // expect(instance).toBeTruthy();
-    expect(instance.username).toBe('');
+    // expect(instance.username).toBe('');
+  });
+
+  test('ID는 이메일 형식이어야 한다.', () => {
+    const wrapper = shallowMount(LoginForm);
+    //  find()로 요소 찾기
+    const idInput = wrapper.find('#username', {
+      data() {
+        return {
+          username: ' test',
+        };
+      },
+    });
+    console.log(idInput.html());
+    console.log(idInput.element.value);
+    console.log(wrapper.vm.isUsernameValid);
+    // wrapper의 경우 있는 경우에는 Wrapper{} 으로 출력될것이고, 없으면 에러를 뱉는다.
+
+    const warningText = wrapper.find('.warning');
+    console.log(warningText.html());
+    expect(warningText.exists()).toBeTruthy(); // exists있으면 true / 없으면 false . toBetruthy true/false 확인
+  });
+
+  test('ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화 된다.', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: '',
+          password: '',
+        };
+      },
+    });
+    const button = wrapper.find('button');
+    expect(button.element.disabled).toBeTruthy();
   });
 });
